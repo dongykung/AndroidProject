69dbc9fcf5549068da9c5a67e6857e8fda40eaaa
dongykung <92030316+dongykung@users.noreply.github.com>
2022-11-27 04:18:04 +0900

    test

--- app/src/main/java/com/example/snsandroid/HomeActivity.kt
+++ app/src/main/java/com/example/snsandroid/HomeActivity.kt
@@ -1,10 +1,16 @@
 package com.example.snsandroid
 
 import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.snsandroid.Navigation.AddPhotoAcitivty
+import com.example.snsandroid.databinding.ActivityAddPhotoBinding
 import com.example.snsandroid.databinding.ActivityHomeBinding
 
 
@@ -34,7 +40,11 @@ class HomeActivity: AppCompatActivity() {
                }
                R.id.action_add_photo ->{
                    println("사진추가")
-
+                   if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                       startActivity(Intent(this, AddPhotoAcitivty::class.java))
+                   } else {
+                       Toast.makeText(this, "스토리지 읽기 권한이 없습니다.", Toast.LENGTH_LONG).show()
+                   }
                    return@setOnItemSelectedListener true
                }
                R.id.action_favorite_alarm ->{
